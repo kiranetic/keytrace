00558a74d2bdcc6b9252fce9567bce4091a1ee78
kiranetic <kirankumar.rathod@outlook.com>
2025-04-11 10:08:10 +0000

    Moved out prompt_builder

---
+++ utils.py
@@ -0,0 +1,16 @@
+def prompt_builder(content, keyword):
+    prompt = (
+        f'''
+        You are a helpful keyword scrapper assistant. 
+        From the provided web content, extract a sentence that is either directly about the given keyword or closely related/relevent to it. 
+        Don't rephrase or mention things not discussed in the content.
+        Strictly base your response on the content.
+
+        Keyword: {keyword}
+
+        Content:
+        {content}
+        '''
+    )
+
+    return prompt
